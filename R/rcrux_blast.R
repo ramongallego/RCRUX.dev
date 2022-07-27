cb631db335ed9ca37afe5440cfa6d42d9067346b
Luna <monstersmakemehorny@gmail.com>
2022-07-27 17:54:26 -0400

    added trailing / to dir names. Surely there is a better way to do this?

--- R/rcrux_blast.R
+++ R/rcrux_blast.R
@@ -15,8 +15,8 @@
 #' @export
 rcrux_blast <- function(seeds_path, db_dir, accession_taxa_path, working_dir,
                         expand_vectors = TRUE, ...) {
-    output_dir <- paste(working_dir, "rcrux_blast_output", sep = "/")
-    save_dir <- paste(working_dir, ".rcrux_blast_save", sep = "/")
+    output_dir <- paste(working_dir, "rcrux_blast_output/", sep = "/")
+    save_dir <- paste(working_dir, ".rcrux_blast_save/", sep = "/")
     dir.create(working_dir)
     dir.create(save_dir)
     dir.create(output_dir)
