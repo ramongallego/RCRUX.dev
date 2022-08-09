aa43ff187265c3e83eee51a5956704876ae222ba
Luna <monstersmakemehorny@gmail.com>
2022-08-09 16:27:36 -0400

    run_blastdbcmd file name corrected. The function itself was changed, making some tweaks for clarity. Additionally, its database extracting function was entirely removed, with the function now simply taking a db argument.

--- R/run_blastdbcmd.R
+++ R/run_blastdbcmd.R
@@ -2,28 +2,19 @@
 #'
 #' Given a row from a blast_seed formatted data.frame, extracts arguments
 #' for blastdbcmd, then returns the output of the blastdbcmd call.
-#' 
+#'
 #' @param row a row from get_blast_seeds
-#' @param db_dir a directory with a blast-formatted database
+#' @param db a directory with a blast-formatted database
 #' @param ncbi_bin if not null use it as the parent directory for blastn
 #' @return a fasta-formatted character vector
 #' @export
-run_blastdbcmd <- function(query_row, db_dir, ncbi_bin = NULL) {
+run_blastdbcmd <- function(query_row, db, ncbi_bin = NULL) {
     # Extract arguments
     accession <- query_row$accession
-    db <- query_row$database
-    if (db ==  "refseq_representative_genomes") {
-        if (query_row$superkingdom == "Eukaryota") {
-            db <- "ref_euk_rep_genomes"
-        }
-        else {
-            db <- "ref_prok_rep_genomes"
-        }
-    }
-    db_path <- paste(db_dir, db, sep = "/")
-    db_type <- "nucl"
     forward <- query_row$forward_stop
     reverse <- query_row$reverse_stop
+
+    # Massage forward and reverse
     if (forward < reverse) {
         forward <- forward + 1
         reverse <- reverse - 1
@@ -40,23 +31,23 @@ run_blastdbcmd <- function(query_row, db_dir, ncbi_bin = NULL) {
         reverse <- reverse - 1
     }
 
+    seq_range <- paste0(forward, "-", reverse)
+
     # System call
     if (is.null(ncbi_bin)) {
-        fasta <- system2("blastdbcmd", args = c("-db", db_path,
-                                                "-dbtype", db_type,
+        fasta <- system2("blastdbcmd", args = c("-db", db,
+                                                "-dbtype", "nucl",
                                                 "-entry", accession,
-                                                "-range",
-                                                paste0(forward, "-", reverse)),
+                                                "-range", seq_range),
                                                 stdout = TRUE)
         return(fasta)
     }
     else {
         blastdbcmd <- paste(ncbi_bin, "blastdbcmd", sep = "/")
-        fasta <- system2(blastdbcmd, args = c("-db", db_path,
-                                                "-dbtype", db_type,
+        fasta <- system2(blastdbcmd, args = c("-db", db,
+                                                "-dbtype", "nucl",
                                                 "-entry", accession,
-                                                "-range",
-                                                paste0(forward, "-", reverse)),
+                                                "-range", seq_range),
                                                 stdout = TRUE)
         return(fasta)
     }
