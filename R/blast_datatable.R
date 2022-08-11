b9746c64960fa93f83d00db99fdfa4b877fac4b5
Luna <monstersmakemehorny@gmail.com>
2022-08-11 15:30:06 -0400

    Added a progress bar. Hope it works!

--- R/blast_datatable.R
+++ R/blast_datatable.R
@@ -96,6 +96,7 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
         # run blastdbcmd on each
         # sort results into appropriate buckets
         aggregate_fasta <- NULL
+        pb <- progress::progress_bar$new(total = length(sample_indices))
         for (index in sample_indices) {
             fasta <- run_blastdbcmd(blast_seeds[index, ], db)
             # Maybe in these cases we can just append directly to output?
@@ -114,6 +115,7 @@ blast_datatable <- function(blast_seeds, save_dir, db, accession_taxa_path,
             else {
                 aggregate_fasta <- append(aggregate_fasta, fasta)
             }
+            pb$tick()
         }
 
         # run blastn and aggregate results
